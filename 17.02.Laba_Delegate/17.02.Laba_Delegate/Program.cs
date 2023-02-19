9c5022972de7a58b3d75a85c6d1b1c091eb9513a
Nikol <nikolmotorina@gmail.com>
2023-02-19 22:12:05 +0200

    19.02. HW

--- 17.02.Laba_Delegate/17.02.Laba_Delegate/Program.cs
+++ 17.02.Laba_Delegate/17.02.Laba_Delegate/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace _17._02.Laba_Delegate
 {
+    //классы
     internal class Program
     {
         class Out
@@ -75,9 +76,123 @@ namespace _17._02.Laba_Delegate
                 return false;
             }
         }
+        class Array
+        {
+            int[] arr;
+            public Array()
+            {
+                arr = new int[0];
+            }
+            public Array(int size)
+            {
+                arr = new int[size];
+            }
+            public Array(int[] arr)
+            {
+                this.arr = arr;
+            }
+            public int this[int ind]
+            {
+                get
+                {
+                    if (ind >= 0 && ind < arr.Length) 
+                        return arr[ind];
+                    else throw new IndexOutOfRangeException();
+                }
+                set
+                {
+                    if (ind >= 0 && ind < arr.Length)
+                        arr[ind] = value;
+                    else throw new IndexOutOfRangeException();
+                }
+            }
+            public void Even()
+            {
+                Console.WriteLine("Even elements:");
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 == 0) Console.WriteLine(arr[i]);
+                }
+            }
+            public void Odd()
+            {
+                Console.WriteLine("Odd elements:");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 != 0) Console.WriteLine(arr[i]);
+                }
+            }
+            public void Prime()
+            {
+                Console.WriteLine("Prime elements:");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > 1)
+                    {
+                        for (int k = 2; k < arr[i] - 1; i++)
+                        {
+                            if (arr[i] % k == 0) continue;
+                        }
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+            public void Fibonacci()
+            {
+                Console.WriteLine("Fibonacci elements:");
+
+                int a = 0;
+                int b = 1;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    while (a <= arr[i])
+                    {
+                        int temp = a;
+                        a = b;
+                        b = temp + b;
+                        if (a == arr[i]) Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+            public void Print()
+            {
+                Console.Write("Array: ");
+                foreach(int i in arr) Console.WriteLine(i + " ");
+            }
+        }
+        class Time
+        {
+            DateTime date;
+            public void GetTime()
+            {
+                date = DateTime.Now;
+                Console.Write("Time: ");
+                Console.WriteLine(date.Hour + ":" + date.Minute + ":" + date.Second);
+            }
+            public void GetDate()
+            {
+                date = DateTime.Now;
+                Console.Write("Date: ");
+                Console.WriteLine(date.Year + "." + date.Month + "." + date.Day);
+            }
+            public void GetDayOfWeek()
+            {
+                date = DateTime.Now;
+                Console.Write("Day of week: ");
+                Console.WriteLine(date.DayOfWeek);
+            }
+        }
+
+        //делегаты
+
         delegate void GetAsStrin(string str);
         delegate int GetAsInt();
         delegate bool GetAsBool();
+        delegate void GetAsArr();
+        delegate void GetAsDate();
+
+        //выполнения заданий
+
         static void Task_1()
         {
             Out str = new Out();
@@ -120,12 +235,49 @@ namespace _17._02.Laba_Delegate
                     Console.WriteLine("Result: " + d_bool[choice - 1]());
                 }
             }
+        }
+        static void HW_1()
+        {
+            int[] array = { 1, 5, 3, 9, 4 };
+            Array arr = new Array(array);
 
+            GetAsArr[] d_arr = { arr.Even, arr.Odd, arr.Prime, arr.Fibonacci, arr.Print };
+            int choice = 0;
+
+            while (choice != 6)
+            {
+                Console.Write("\n1 Even\n2 Odd\n3 Prime\n4 Fibonacci\n5 Print\n6 Exit\nYour choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice >= 1 && choice <= 5)
+                {
+                    d_arr[choice - 1]();
+                }
+            }
+        }
+        static void HW_2()
+        {
+            Time t = new Time();
+            GetAsDate[] d = { t.GetTime, t.GetDate, t.GetDayOfWeek };
+            int choice = 0;
+
+            while (choice != 4)
+            {
+                Console.Write("\n1 Get Time\n2 Get Date\n3 Get Day Of Week\n4 Exit\nYour choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice >= 1 && choice <= 3)
+                {
+                    d[choice - 1]();
+                }
+            }
         }
         static void Main(string[] args)
         {
             //Task_1();
-            Task_2_3();
+            //Task_2_3();
+            //HW_1();
+            HW_2();
         }
     }
 }
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(50, 30);
+
+            Console.WriteLine(
+                    "##################################################" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "#                                                #" +
+                    "##################################################");
+
+            //Console.
+            Console.ReadKey();
+        }
+    }
+}

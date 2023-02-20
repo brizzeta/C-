d58e8197a00a59338f713d7c2c6bf312a80dd875
Nikol <nikolmotorina@gmail.com>
2023-02-20 15:27:16 +0200

    20.02. HW

---
+++ 20.02.Events/20.02.Events/Credit_Card.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._02.Events
+{
+    delegate void MyDelegate();
+    internal class Credit_Card
+    {
+        List<MyDelegate> list = new List<MyDelegate>();
+        public string Full_Name { get; set; }
+        public string Term { get; set; }
+        public int PIN { get; set; }
+        public float Limit { get; set; }
+        public float Cache { get; set; }
+        public Credit_Card(string full_Name, string term, int pIN, float limit, float cache)
+        {
+            Full_Name = full_Name;
+            Term = term;
+            PIN = pIN;
+            Limit = limit;
+            Cache = cache;
+        }
+        public override string ToString() => $"Full name: {Full_Name}\nTerm of use: {Term}\nPIN: {PIN}\nCredir limit: {Limit}\nCache: {Cache}";
+        public event MyDelegate ev
+        {
+            add {  list.Add(value); }
+            remove { list.Remove(value); }
+        }
+        public MyDelegate this[int ind]
+        {
+            get
+            {
+                if (ind >= 0 && ind < list.Count)
+                    return list[ind];
+                else throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (ind >= 0 && ind < list.Count)
+                    list[ind] = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+        public void Event()
+        {
+            if(list.Count != 0)
+            {
+                foreach (MyDelegate el in list) el();
+            }
+        }
+        public void AddCache()
+        {
+            Console.Write("Enter amount of cache to add: ");
+            float cache = float.Parse(Console.ReadLine());
+            Cache += cache;
+        }
+        public void RemoveCache()
+        {
+            Console.Write("Enter amount of cache to remove: ");
+            float cache = float.Parse(Console.ReadLine());
+            Cache -= cache;
+        }
+        public void Start()
+        {
+            Console.WriteLine("Start of using the credit card.");
+        }
+        public void Check() 
+        {
+            Console.Write("Enter amount of cache to accumulated: ");
+            float cache = float.Parse(Console.ReadLine());
+
+            if (Cache >= cache)
+                Console.WriteLine("Amount accumulated.");
+            else Console.WriteLine("Amount NOT accumulated.");
+        }
+        public void ChangePIN()
+        {
+            Console.Write("Enter new PIN: ");
+            int pin = int.Parse(Console.ReadLine());
+            PIN = pin;
+        }
+    }
+}
---
+++ 20.02.Events/20.02.Events/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._02.Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Credit_Card card = new Credit_Card("Tatiana Nezina Evgenia", "2025.09.01", 2727, 2000, 900);
+
+            int result = 0;
+            while(result != 6)
+            {
+                Console.Write("MENU\n\n1 Add cache\n2 Remove cache\n3 Start using card\n4 Check accumulate\n5 Change PIN\n6 Exit\n\nYour variant: ");
+                result = int.Parse(Console.ReadLine());
+
+                if (result == 6) break;
+                
+                switch(result)
+                {
+                    case 1:
+                        card.ev += new MyDelegate(card.AddCache);
+                        card.Event();
+                        card.ev -= new MyDelegate(card.AddCache);
+                        break;
+                    case 2:
+                        card.ev += new MyDelegate(card.RemoveCache);
+                        card.Event();
+                        card.ev -= new MyDelegate(card.RemoveCache);
+                        break;
+                    case 3:
+                        card.ev += new MyDelegate(card.Start);
+                        card.Event();
+                        card.ev -= new MyDelegate(card.Start);
+                        break;
+                    case 4:
+                        card.ev += new MyDelegate(card.Check);
+                        card.Event();
+                        card.ev -= new MyDelegate(card.Check);
+                        break;
+                    case 5:
+                        card.ev += new MyDelegate(card.ChangePIN);
+                        card.Event();
+                        card.ev -= new MyDelegate(card.ChangePIN);
+                        break;
+                }
+                Console.WriteLine("\n" + card.ToString() + "\n");
+            }
+        }
+    }
+}

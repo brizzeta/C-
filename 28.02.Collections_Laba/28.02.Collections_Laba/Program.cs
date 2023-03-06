ba81013df84b8e858f030b7bf103f94a14289509
Nikol <nikolmotorina@gmail.com>
2023-03-06 22:47:21 +0200

    28.02.Laba

---
+++ 28.02.Collections_Laba/28.02.Collections_Laba/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._02.Collections_Laba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Enter text");
+            text = Console.ReadLine();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            string[] text2 = text.Split(' ');
+            foreach (string s in text2)
+            {
+                if (dict.ContainsKey(s))
+                {
+                    dict[s]++;
+                }
+                else
+                {
+                    dict.Add(s, 1);
+                }
+            }
+            foreach (KeyValuePair<string, int> pair in dict)
+                Console.WriteLine($"{pair.Key} {pair.Value}");
+        }
+    }
+}

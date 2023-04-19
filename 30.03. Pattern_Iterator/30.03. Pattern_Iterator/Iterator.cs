cf0ed8ada28506201092e5ac6c2563b5e13c78f5
Nikol <nikolmotorina@gmail.com>
2023-04-20 00:56:23 +0300

    30.03.HW

---
+++ 30.03. Pattern_Iterator/30.03. Pattern_Iterator/Iterator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _30._03.Pattern_Iterator
+{
+    internal class ConcreteIterator : Iterator
+    {
+        ConcreteCollection collection;
+        string iterationState;
+        public ConcreteIterator(ConcreteCollection collection)
+        {
+            this.collection = collection;
+            iterationState = collection.Potemkinski_Stairs;
+        }
+        public void getNext()
+        {
+            switch(iterationState)
+            {
+                case "Potemkinski stairs":
+                    iterationState = collection.Opera_theater;
+                    break;
+                case "Opera theater":
+                    iterationState = collection.Deribasivska; 
+                    break;
+                default: 
+                    if(!hasMore())
+                    {
+                        Console.WriteLine("That's finish!");
+                    }
+                    break;
+            }
+        }
+        public bool hasMore()
+        {
+            if(iterationState == "Deribasivska")
+                return false;
+            return true;
+        }
+    }
+    internal class ConcreteCollection : IterableCollection
+    {
+        public string Potemkinski_Stairs { get; }
+        public string Opera_theater { get; }
+        public string Deribasivska { get; }
+        public ConcreteCollection() 
+        {
+            Potemkinski_Stairs = "Potemkinski stairs";
+            Opera_theater = "Opera theater";            
+            Deribasivska = "Deribasivska";
+        }
+        public Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+    }
+}
---
+++ 30.03. Pattern_Iterator/30.03. Pattern_Iterator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _30._03.Pattern_Iterator
+{
+    internal interface Iterator
+    {
+        void getNext();
+        bool hasMore();
+    }
+    internal interface IterableCollection
+    {
+        Iterator CreateIterator();
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

53dde5b20b888cff93f5e14ac17e94dd7ef9db3d
Nikol <nikolmotorina@gmail.com>
2023-04-21 11:46:54 +0300

    03.04.HW

---
+++ 03.04.Pattern_Strategy/03.04.Pattern_Strategy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._04.Pattern_Strategy
+{
+    internal class Program
+    {
+        internal class Context
+        {
+            public Strategy strategy { get;  set; }
+            public void Time(string strat)
+            {
+                strategy.execute(strat);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 03.04.Pattern_Strategy/03.04.Pattern_Strategy/Strategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._04.Pattern_Strategy
+{
+    internal interface Strategy
+    {
+        void execute(string strat);
+    }
+    internal class ConcreteStrategy : Strategy
+    {
+        double time;
+        public void execute(string strat) 
+        { 
+            switch(strat)
+            {
+                case "Bicycle":
+                    time = 30;
+                    break;
+                case "Bus":
+                    time = 15;
+                    break;
+                case "Taxi":
+                    time = 10;
+                    break;
+            }
+            Console.WriteLine("Time to airport: ", time);
+        }
+    }
+}
